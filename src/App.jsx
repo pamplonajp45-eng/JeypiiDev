4ec1ac0b360e1e80b2e042a70941b0d8441b5661
JPDEV <pamplonajp.45@gmail.com>
2025-12-30 08:11:30 +0800

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Experience from "./components/Experience";
-import Portfolio from "./components/Portfolio";
-import Contact from "./components/Contact";
 import "./App.css";
 import Footer from "./components/Footer";
 
